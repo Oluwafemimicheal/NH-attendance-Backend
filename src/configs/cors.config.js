28712aee7d9474d56655665a7e85502396a104b9
oluwafemi michael <femi4410@gmail.com>
2026-09-25 20:56:28 +0100

    initial commit

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,4 +1,3 @@
-const cors = require('cors');
 
 const allowedOrigins = [
   'http://localhost:3232/',   
--- src/server.js
+++ src/server.js
@@ -31,6 +31,6 @@ app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Service currently running..." })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
